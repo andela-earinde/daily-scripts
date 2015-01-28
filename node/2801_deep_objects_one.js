a6488e2b3b9375f394cf01d29764dd17a128bd6f
gnerkus <ifeanyioraelosi@gmail.com>
2015-01-28 11:34:03 +0100

    Improve deep search function.

--- node/2801_deep_objects_one.js
+++ node/2801_deep_objects_one.js
@@ -1,11 +1,18 @@
 function deepObjects (object, property) {
-  if (typeof object[property] !== 'object') {
-    return object[property];
-  } else {
-    return deepObjects(object[property], property);
+  for (var key in Object.keys(object)) {
+    var value = Object.keys(object)[key];
+    if (object.hasOwnProperty(value)) {
+      if (typeof object[value] === 'object') {
+        return deepObjects(object[value], property);
+      } else if (value === property) {
+        return object[value];
+      } else {
+        return "Property not found.";
+      }
+    }
   }
 }
 
 var object = JSON.parse(process.argv[2]);
-var property = process.argv[3][0];
+var property = process.argv[3];
 console.log(deepObjects(object, property));

d1f00d9cfeeab8488f0886c5806d2f4cdb3664c4
gnerkus <ifeanyioraelosi@gmail.com>
2015-02-06 15:17:40 +0100

    Add powerset function

---
+++ node/0502_power_set.js
@@ -0,0 +1,32 @@
+function powerSet (array) {
+  var result = [];
+
+  for (var i = 0; i < (Math.pow(2, array.length)); i++) {
+    var binaryRep = i.toString(2);
+    binaryRep = padBinary(binaryRep, array.length);
+    var set = [];
+
+    for (var key in binaryRep) {
+      if (binaryRep[key] === "1") {
+        set.push(array[key]);
+      }
+    }
+
+    result.push(set);
+  }
+
+  return result;
+}
+
+function padBinary (binary, stringLength) {
+  var len = binary.length;
+  var rem = stringLength - binary.length;
+
+  var padding = [];
+  for (var i = 0; i < rem; i++) {
+    padding.push(0);
+  }
+  padding.join("");
+
+  return padding + binary;
+}
